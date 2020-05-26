b76d193735f41918ae34d239ce40061f62e2b03d
osagie obaretin <osagie.codes@gmail.com>
2020-05-26 14:39:06 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+  return string.toUpperCase() 
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO')
+}
+function logWhisper(string) {
+  console.log('hello')
+}
+function sayHiToGrandma(string) {
+  return string.toLowerCase("I can't hear you!")
+  return string.toUpperCase("YES INDEED!")
+  return string.toIloveYouGrandma;
+}
